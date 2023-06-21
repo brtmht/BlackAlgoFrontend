bd2fdc27fc944f5f73428c08012f8375641f6df3
geekiscode <codeaj900@gmail.com>
2023-06-21 19:19:09 +0530

    changes in get user exchange  query

--- src/routes/v1/userExchangeConfig.route.js
+++ src/routes/v1/userExchangeConfig.route.js
@@ -45,6 +45,7 @@ module.exports = router;
  *                 type: string
  *             example:
  *               exchangeId: 646c543f6086f579abe86b58
+ *               strategyId: 646c543f6086f579abe86b58
  *               config:
  *                   login: 260050728
  *                   password: abc
