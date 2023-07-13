3aada76b9f14dc8d3cb12b81e73c044eda21e9a0
geekiscode <codeaj900@gmail.com>
2023-07-13 15:34:50 +0530

    binance pay api fix

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -14,13 +14,13 @@ const PaymentDetail = require('../models/paymentDetail.model');
 
 // binanace API
 const getBinance = catchAsync(async (req, res) => {
- // const binanceData = await binanceService.createBinancePayOrder(req.user._id, req.body);
+  // const binanceData = await binanceService.createBinancePayOrder(req.user._id, req.body);
   res.send();
 });
 // post binanace
 const postBinance = catchAsync(async (req, res) => {
   const binanceData = await binanceService.createBinancePayOrder(req.user._id, req.body);
-  console.log(binanceData,"==============binanceData");
+  console.log(binanceData, '==============binanceData');
   res.send(binanceData);
 });
 // // log in binance
@@ -28,20 +28,18 @@ const binanceWebhook = catchAsync(async (req, res) => {
   console.log(req.body, '===========req.body');
   if (req.body) {
     if (req.body.bizType === 'PAY') {
-      if(req.body.bizStatus === 'PAY_SUCCESS'){
+      if (req.body.bizStatus === 'PAY_SUCCESS') {
         await transactionHistoryService.saveBinanceTransactionHistory(req.body);
       }
-      if(req.body.bizStatus === 'PAY_CLOSED'){
+      if (req.body.bizStatus === 'PAY_CLOSED') {
         await paymentDetailService.updateBinancePaymentDetails(req.body);
       }
       res.send({
-        "returnCode":"SUCCESS",
-        "returnMessage":null
-       }); 
+        returnCode: 'SUCCESS',
+        returnMessage: null,
+      });
     }
-    
   }
-  
 });
 // stripe config
 const getStripeConfig = catchAsync(async (req, res) => {
@@ -57,7 +55,9 @@ const stripeWebhook = catchAsync(async (req) => {
 const createPayment = catchAsync(async (req, res) => {
   // let paymentData;
   const user = req.user._id;
-
+  if (req.body.paymentType !== 'crypto' && req.body.paymentType !== 'card') {
+    res.send({ success: false, error_code: 403, message: 'payment type not valid' });
+  }
   if (req.body.paymentType === 'card') {
     const paymentData = await stripeAccountService.createStripePayment(req.body, user);
     if (paymentData) {
@@ -70,24 +70,24 @@ const createPayment = catchAsync(async (req, res) => {
     res.send({
       success: true,
       code: 201,
-      message: 'stripe token created Successfully',
+      message: 'crypto order created Successfully',
       data: { stripe_token: paymentData.paymentIntent.client_secret },
     });
   }
   if (req.body.paymentType === 'crypto') {
     const binanceData = await binanceService.createBinancePayOrder(req.user._id, req.body);
-    console.log(binanceData,"-------------");
-    if(binanceData.status === 'SUCCESS'){
+    if (binanceData.status === 'SUCCESS') {
       res.send({
         success: true,
         code: 201,
         message: 'stripe token created Successfully',
         data: { BinanceData: binanceData },
       });
+    } else {
+      res.send({ success: false, error_code: 400, message: 'Something wrong' });
     }
-    res.send({"success": false,"error_code": 400,"message": "Something wrong"});
   }
-  res.send({"success": false,"error_code": 403,"message": "payment type not valid"});
+ 
 });
 
 // save data in transaction table
