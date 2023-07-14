b1d511cdc6f2579f7d9f804cac6c6eac7d97a8ff
geekiscode <codeaj900@gmail.com>
2023-07-14 13:18:14 +0530

    share image upload api

--- src/middlewares/imageUpload.js
+++ src/middlewares/imageUpload.js
@@ -3,6 +3,9 @@ const multer = require('multer');
 const imageUploadMiddleware = (dynamicePath) => {
   const storage = multer.diskStorage({
     destination: (req, file, cb) => {
+      console.log('====================================');
+      console.log(file);
+      console.log('====================================');
       cb(null, `./public/${dynamicePath}/`);
     },
     filename: (req, file, cb) => {
